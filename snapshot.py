1e62520086f5633f7bfd79edd738eaffebb83266
Rino Jose <rjose@linkedin.com>
2013-09-24 12:57:54 -0700

    Pull generic functions out

--- snapshot.py
+++ snapshot.py
@@ -2,76 +2,137 @@
 
 import io
 import os
+import re
 import subprocess
 
 import zmq
 
 from utils.sectionize import sectionize
 
+class CommitError(Exception):
+    pass
+
+#TODO: Add functions to create stacked streams (these will eventually go into sectionize)
+
+
+# TODO: Read these ports from the commandline
+reply_port = 5000
+publish_port = 5100
+
+# Create 0mq context
 context = zmq.Context()
 
-# TODO: Clean up this file with malice
+# Socket for replying to snapshot requests from users (GETs and PUTs)
+rep_socket = context.socket(zmq.REP)
+rep_socket.bind("tcp://127.0.0.1:%d" % reply_port)
 
-# This is a socket for replying to requests for snapshot data
-socket = context.socket(zmq.REP)
-socket.bind("tcp://127.0.0.1:5000")
-
+# Socket for publishing changes to resources
 pub_socket = context.socket(zmq.PUB)
-pub_socket.bind("tcp://127.0.0.1:5100")
+pub_socket.bind("tcp://127.0.0.1:%d" % publish_port)
 
-#TODO: Add functions to create stacked streams
+# TODO: Set this from the commandline (default to current dir)
+working_directory = "/home/rjose/dev/snapshot/temp"
+os.chdir(working_directory)
 
-while True:
-    msg = socket.recv()
-    input = io.StringIO(msg)
-    sections = sectionize(input)
+def commit_file(file):
+    # Add file
+    git_result = subprocess.call("git add %s" % file, shell=True)
+    if git_result != 0:
+        raise CommitError("Problem adding %s" % file)
 
-    # TODO: Move this to its own function
-    # TODO: Rename "PUT raw qplan"
-    if "PUT raw qplan" in sections:
-        raw_qplan = sections["PUT raw qplan"]
-        # Create file
-        if not os.path.exists("./raw"):
-            os.makedirs("./raw")
-        file = open("./raw/qplan.txt", "w")
-        file.write(raw_qplan);
-        file.close()
+    # Commit file
+    git_result = subprocess.call("git commit -m 'Update %s'" % file, shell=True)
 
-        # Add file and check in
-        git_result = subprocess.call("git add ./raw/qplan.txt", shell=True)
-        if git_result != 0:
-            socket.send("ERROR: Couldn't add raw/qplan.txt")
-            raise Exception("Problem adding raw/qplan.txt")
-        git_result = subprocess.call("git commit -m 'Update raw qplan data'",
-                shell=True)
-        # TODO: Check if file hasn't changed
-        #if git_result != 0:
-        #    socket.send("ERROR: Couldn't commit raw/qplan.txt")
-        #    raise Exception("Problem commiting raw/qplan.txt")
+    # TODO: Check if file hasn't changed
+    #if git_result != 0:
+    #    socket.send("ERROR: Couldn't commit raw/qplan.txt")
+    #    raise Exception("Problem commiting raw/qplan.txt")
 
-        # Announce that there's new raw data
-        pub_socket.send("=====qplan raw");
 
-        socket.send("OK")
+def put_resource(header, data, header_map):
+    resource = header.split("PUT ")[1]
+    filename = header_map[resource]
+    dirname = os.path.dirname(filename)
+    # TODO: Test this at least once
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
 
-    elif "GET raw qplan" in sections:
-        version = sections["GET raw qplan"].split("\t")[0]
-        if not version:
-            version = "HEAD"
+    # Write data to file
+    file = open(filename, "w")
+    file.write(data);
+    file.close()
 
-        p = subprocess.Popen("git show %s:raw/qplan.txt" % version,
-                stdout=subprocess.PIPE, shell=True)
-        input = io.StringIO(p.communicate()[0])
+    # Check file in
+    try:
+        commit_file(filename)
 
-        p = subprocess.Popen("git rev-parse %s" % version,
-                stdout=subprocess.PIPE, shell=True)
-        rev = p.communicate()[0][0:5]
+        # Publish that file was checked in
+        pub_socket.send_unicode("=====%s" % resource);
+        print(resource)
 
-        header = ["=====raw qplan\n\t%s\n" % rev, "=====data\n"]
-        data = []
-        for l in input.readlines():
-            data.append("\t%s" % l)
-        message = "".join(header + data)
-        socket.send_unicode(message)
-    else:
-        socket.send("ERROR: Unable to process any: %s" % sections.keys())
+        # Reply to client that PUT file
+        rep_socket.send("OK")
+    except CommitError:
+        rep_socket.send_unicode("ERROR: Couldn't commit PUT %s" % resource)
+
+# TODO: Move this to sectionize
+def make_sections(section_lists):
+    sections = []
+    for sls in section_lists:
+        header = "=====%s\n" % sls[0]
+        data = sls[1]
+        sections.append(header + data)
+    result = "".join(sections)
+    return result
+
+
+def get_resource(header, data, header_map):
+    resource = header.split("GET ")[1]
+    filename = header_map[resource]
+    version = data.split("\t")[0]
+    if not version:
+        version = "HEAD"
+    print("Getting data for %s, %s" % (header, version))
+
+    p = subprocess.Popen("git show %s:%s" % (version, filename),
+            stdout=subprocess.PIPE, shell=True)
+    contents = io.StringIO(p.communicate()[0])
+
+    p = subprocess.Popen("git rev-parse %s" % version,
+            stdout=subprocess.PIPE, shell=True)
+    rev = p.communicate()[0][0:5]
+
+    data = []
+    for l in contents.readlines():
+        data.append("\t%s" % l)
+    new_contents = "".join(data)
+    message = make_sections([["raw qplan", "\t%s\n" % rev], ["data", new_contents]])
+    rep_socket.send_unicode(message)
+
+
+def event_loop(header_map):
+    while True:
+        message = io.StringIO(rep_socket.recv())
+        # TODO: Handle errors in sectionizing
+        sections = sectionize(message)
+
+        for header in sections.keys():
+            if re.match("PUT", header):
+                put_resource(header, sections[header], header_map)
+            elif re.match("GET", header):
+                get_resource(header, sections[header], header_map)
+            else:
+                print(header)
+                rep_socket.send_unicode("TODO: Handle %s" % header)
+
+
+#===============================================================================
+# Code that should be in a custom script
+#
+header_file_map = {
+        "qplan raw": "qplan_raw.txt",
+        "qplan cond": "qplan_cond.txt",
+        "qplan app": "qplan_app.txt"
+}
+event_loop(header_file_map)
+
